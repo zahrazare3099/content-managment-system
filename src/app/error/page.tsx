51465ab76dcd101310788d43211792aa92676d64
ZahraZare <zahrazare3099@gmail.com>
2025-02-09 21:03:14 +0330

    [ADD} error page

---
+++ src/app/error/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function ErrorPage(error: Error) {
+  return (
+    <div className="w-full flex flex-col justify-center  gap-y-4">
+      <p>Sorry, something went wrong</p>
+      <p className="text-red-500 font-bold">{error.message}</p>
+    </div>
+  );
+}

51f44d324cc10d4c9abc114de3887d54190b6b8f
ZahraZare <zahrazare3099@gmail.com>
2025-02-14 00:51:41 +0330

    [FIX] user session

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import getUserSession from "./_actions/getUserSession";
+import { redirect } from "next/navigation";
 
 type FormLayoutProps = {
   children: React.ReactNode;
 };
 
-export default function layout({ children }: FormLayoutProps) {
+export default async function layout({ children }: FormLayoutProps) {
+  const response = await getUserSession();
+  if (response?.user) {
+    redirect("/");
+  }
   return (
     <div className="w-full h-screen flex justify-center items-start mt-12">
       {children}

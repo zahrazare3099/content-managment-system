71caa05fbb980e5ceff4f10270e656b02e2e2ae1
ZahraZare <zahrazare3099@gmail.com>
2025-02-13 19:43:44 +0330

    [CREATE] logout page , component, actions

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,3 +1,23 @@
-export function Header() {
-  return <div className="bg-slate-300 p-5 w-full"></div>;
+import LogoutButton from "@/app/(auth)/_components/LogoutButton";
+import { createClient } from "@/utiles/supabase/server";
+import Link from "next/link";
+
+export async function Header() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  return (
+    <div className="bg-slate-300 w-full p-3 px-5 flex flex-row-reverse">
+      <span>
+        {user ? (
+          <LogoutButton />
+        ) : (
+          <Link href="/login" className="hover:text-blue-500">
+            👤 ورود به حساب کاربری
+          </Link>
+        )}
+      </span>
+    </div>
+  );
 }

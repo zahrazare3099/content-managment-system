3cc4743dac7ad5579f9ccb7fb32fb03b65149bed
ZahraZare <zahrazare3099@gmail.com>
2025-03-15 17:43:26 +0330

    .

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className="home_page  h-full w-full flex">
-      <h1 className="p-5">home page</h1>
+    <div className="home_page  h-full w-full flex flex-col p-5">
+      <h1>home page</h1>
+      <p>This is the initial content</p>
     </div>
   );
 }

a44cea3ec123e9fc2d6dd678a8a4d2bc656215fc
ZahraZare <zahrazare3099@gmail.com>
2025-03-18 16:50:02 +0330

    b-one changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="home_page  h-full w-full flex flex-col p-5">
       <h1>home page</h1>
-      <p>This is the b-one-test branch content</p>
+      <p>This is the b-one branch content</p>
     </div>
   );
 }

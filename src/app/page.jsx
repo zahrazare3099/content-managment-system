5ff11a01e6e3bc5b01cb3f60aa294456e844d1c3
ZahraZare <zahrazare3099@gmail.com>
2025-03-18 16:15:30 +0330

    master changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,9 +2,7 @@ export default function Home() {
   return (
     <div className="home_page  h-full w-full flex flex-col p-5">
       <h1>home page</h1>
-      <p>This is the b-two content</p>
-      <p>This is the b-one-one content</p>
-      <p>This is the initial-ini content</p>
+      <p>This is the initial content</p>
     </div>
   );
 }
